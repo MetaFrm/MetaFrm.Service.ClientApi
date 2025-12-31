03f049e10a32f6aad81e4333f401d9dd2d34f560
권대선 <dsun@hanmail.net>
2025-12-31 14:50:10 +0900

    GetAwaiter().GetResult()

--- src/ClientApi.cs
+++ src/ClientApi.cs
@@ -24,7 +24,7 @@ namespace MetaFrm.Service
             {
                 PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
                 DefaultIgnoreCondition = System.Text.Json.Serialization.JsonIgnoreCondition.WhenWritingNull,
-                WriteIndented = true,
+                WriteIndented = false,
             };
         }
 
@@ -35,36 +35,14 @@ namespace MetaFrm.Service
 
         Response IService.Request(ServiceData serviceData)
         {
-            try
-            {
-                HttpRequestMessage httpRequestMessage = new(HttpMethod.Post, $"{Factory.BaseAddress}api/Service")
-                {
-                    Headers = {
-                        { HeaderNames.Accept, "application/json" },
-                        { "token", serviceData.Token },
-                    },
-                    Content = new StringContent(JsonSerializer.Serialize(serviceData, JsonSerializerOptions), Encoding.UTF8, "application/json")
-                };
-
-                HttpResponseMessage httpResponseMessage = Factory.HttpClientFactory.CreateClient().SendAsync(httpRequestMessage).Result;
-
-                if (httpResponseMessage.IsSuccessStatusCode)
-                    return httpResponseMessage.Content.ReadFromJsonAsync<Response>().Result ?? new();
-                else 
-                    return new();
-            }
-            catch (Exception exception)
-            {
-                Factory.Logger.LogError(exception, "{Message}", exception.Message);
-                throw new MetaFrmException(exception);
-            }
+            return ((IServiceAsync)this).RequestAsync(serviceData).GetAwaiter().GetResult();
         }
 
         async Task<Response> IServiceAsync.RequestAsync(ServiceData serviceData)
         {
             try
             {
-                HttpRequestMessage httpRequestMessage = new(HttpMethod.Post, $"{Factory.BaseAddress}api/Service")
+                using HttpRequestMessage httpRequestMessage = new(HttpMethod.Post, $"{Factory.BaseAddress}api/Service")
                 {
                     Headers = {
                         { HeaderNames.Accept, "application/json" },
@@ -73,60 +51,39 @@ namespace MetaFrm.Service
                     Content = new StringContent(JsonSerializer.Serialize(serviceData, JsonSerializerOptions), Encoding.UTF8, "application/json")
                 };
 
-                HttpResponseMessage httpResponseMessage = await Factory.HttpClientFactory.CreateClient().SendAsync(httpRequestMessage);
+                using HttpResponseMessage httpResponseMessage = await Factory.HttpClientFactory.CreateClient().SendAsync(httpRequestMessage);
 
                 if (httpResponseMessage.IsSuccessStatusCode)
-                    return await httpResponseMessage.Content.ReadFromJsonAsync<Response>() ?? new();
+                    return await httpResponseMessage.Content.ReadFromJsonAsync<Response>(JsonSerializerOptions) ?? new();
                 else
                     return new();
             }
             catch (Exception exception)
             {
-                Factory.Logger.LogError(exception, "{Message}", exception.Message);
+                if (Factory.Logger.IsEnabled(LogLevel.Error))
+                    Factory.Logger.LogError(exception, "{Message}", exception.Message);
                 throw new MetaFrmException(exception);
             }
         }
 
         UserInfo ILoginService.Login(string email, string password)
         {
-            try
-            {
-                HttpRequestMessage httpRequestMessage = new(HttpMethod.Post, $"{Factory.BaseAddress}api/Login?email={email}&password={password}")
-                {
-                    Headers = {
-                        { HeaderNames.Accept, "application/json" },
-                        { "token", Factory.AccessKey },
-                    },
-                    Content = new FormUrlEncodedContent([new(nameof(email), email), new(nameof(password), password)])
-                };
-
-                HttpResponseMessage httpResponseMessage = Factory.HttpClientFactory.CreateClient().SendAsync(httpRequestMessage).Result;
-
-                if (httpResponseMessage.IsSuccessStatusCode)
-                    return httpResponseMessage.Content.ReadFromJsonAsync<UserInfo>().Result ?? new();
-                else
-                    return new();
-            }
-            catch (Exception exception)
-            {
-                Factory.Logger.LogError(exception, "{Message}", exception.Message);
-                throw new MetaFrmException(exception);
-            }
+            return ((ILoginServiceAsync)this).LoginAsync(email, password).GetAwaiter().GetResult();
         }
         async Task<UserInfo> ILoginServiceAsync.LoginAsync(string email, string password)
         {
             try
             {
-                HttpRequestMessage httpRequestMessage = new(HttpMethod.Post, $"{Factory.BaseAddress}api/Login?email={email}&password={password}")
+                using HttpRequestMessage httpRequestMessage = new(HttpMethod.Post, $"{Factory.BaseAddress}api/Login")
                 {
                     Headers = {
                         { HeaderNames.Accept, "application/json" },
                         { "token", Factory.AccessKey },
                     },
-                    Content = new FormUrlEncodedContent([new(nameof(email), email), new(nameof(password), password)])
+                    Content = JsonContent.Create(new { email, password })
                 };
 
-                HttpResponseMessage httpResponseMessage = await Factory.HttpClientFactory.CreateClient().SendAsync(httpRequestMessage);
+                using HttpResponseMessage httpResponseMessage = await Factory.HttpClientFactory.CreateClient().SendAsync(httpRequestMessage);
 
                 if (httpResponseMessage.IsSuccessStatusCode)
                     return await httpResponseMessage.Content.ReadFromJsonAsync<UserInfo>() ?? new();
@@ -135,42 +92,21 @@ namespace MetaFrm.Service
             }
             catch (Exception exception)
             {
-                Factory.Logger.LogError(exception, "{Message}", exception.Message);
+                if (Factory.Logger.IsEnabled(LogLevel.Error))
+                    Factory.Logger.LogError(exception, "{Message}", exception.Message);
                 throw new MetaFrmException(exception);
             }
         }
 
         string IAccessCodeService.GetJoinAccessCode(string email)
         {
-            try
-            {
-                HttpRequestMessage httpRequestMessage = new(HttpMethod.Get, $"{Factory.BaseAddress}api/AccessCode?email={email}")
-                {
-                    Headers = {
-                        { HeaderNames.Accept, "text/plain" },
-                        { "token", Factory.AccessKey },
-                        { "accessGroup", "JOIN" },
-                    }
-                };
-
-                HttpResponseMessage httpResponseMessage = Factory.HttpClientFactory.CreateClient().SendAsync(httpRequestMessage).Result;
-
-                if (httpResponseMessage.IsSuccessStatusCode)
-                    return httpResponseMessage.Content.ReadAsStringAsync().Result.AesDecryptorToBase64String(Factory.AccessKey, "MetaFrm");
-                else
-                    return "";
-            }
-            catch (Exception exception)
-            {
-                Factory.Logger.LogError(exception, "{Message}", exception.Message);
-                throw new MetaFrmException(exception);
-            }
+            return ((IAccessCodeServiceAsync)this).GetJoinAccessCodeAsync(email).GetAwaiter().GetResult();
         }
         async Task<string> IAccessCodeServiceAsync.GetJoinAccessCodeAsync(string email)
         {
             try
             {
-                HttpRequestMessage httpRequestMessage = new(HttpMethod.Get, $"{Factory.BaseAddress}api/AccessCode?email={email}")
+                using HttpRequestMessage httpRequestMessage = new(HttpMethod.Get, $"{Factory.BaseAddress}api/AccessCode?email={email}")
                 {
                     Headers = {
                         { HeaderNames.Accept, "text/plain" },
@@ -179,7 +115,7 @@ namespace MetaFrm.Service
                     }
                 };
 
-                HttpResponseMessage httpResponseMessage = await Factory.HttpClientFactory.CreateClient().SendAsync(httpRequestMessage);
+                using HttpResponseMessage httpResponseMessage = await Factory.HttpClientFactory.CreateClient().SendAsync(httpRequestMessage);
 
                 if (httpResponseMessage.IsSuccessStatusCode)
                     return await (await httpResponseMessage.Content.ReadAsStringAsync()).AesDecryptorToBase64StringAsync(Factory.AccessKey, "MetaFrm");
@@ -188,42 +124,21 @@ namespace MetaFrm.Service
             }
             catch (Exception exception)
             {
-                Factory.Logger.LogError(exception, "{Message}", exception.Message);
+                if (Factory.Logger.IsEnabled(LogLevel.Error))
+                    Factory.Logger.LogError(exception, "{Message}", exception.Message);
                 throw new MetaFrmException(exception);
             }
         }
 
         string IAccessCodeService.GetAccessCode(string token, string email, string accessGroup)
         {
-            try
-            {
-                HttpRequestMessage httpRequestMessage = new(HttpMethod.Get, $"{Factory.BaseAddress}api/AccessCode?email={email}")
-                {
-                    Headers = {
-                        { HeaderNames.Accept, "text/plain" },
-                        { "token", token },
-                        { "accessGroup", accessGroup },
-                    }
-                };
-
-                HttpResponseMessage httpResponseMessage = Factory.HttpClientFactory.CreateClient().SendAsync(httpRequestMessage).Result;
-
-                if (httpResponseMessage.IsSuccessStatusCode)
-                    return httpResponseMessage.Content.ReadAsStringAsync().Result.AesDecryptorToBase64String(token, "MetaFrm");
-                else
-                    return "";
-            }
-            catch (Exception exception)
-            {
-                Factory.Logger.LogError(exception, "{Message}", exception.Message);
-                throw new MetaFrmException(exception);
-            }
+            return ((IAccessCodeServiceAsync)this).GetAccessCodeAsync(token, email, accessGroup).GetAwaiter().GetResult();
         }
         async Task<string> IAccessCodeServiceAsync.GetAccessCodeAsync(string token, string email, string accessGroup)
         {
             try
             {
-                HttpRequestMessage httpRequestMessage = new(HttpMethod.Get, $"{Factory.BaseAddress}api/AccessCode?email={email}")
+                using HttpRequestMessage httpRequestMessage = new(HttpMethod.Get, $"{Factory.BaseAddress}api/AccessCode?email={email}")
                 {
                     Headers = {
                         { HeaderNames.Accept, "text/plain" },
@@ -232,7 +147,7 @@ namespace MetaFrm.Service
                     }
                 };
 
-                HttpResponseMessage httpResponseMessage = await Factory.HttpClientFactory.CreateClient().SendAsync(httpRequestMessage);
+                using HttpResponseMessage httpResponseMessage = await Factory.HttpClientFactory.CreateClient().SendAsync(httpRequestMessage);
 
                 if (httpResponseMessage.IsSuccessStatusCode)
                     return await (await httpResponseMessage.Content.ReadAsStringAsync()).AesDecryptorToBase64StringAsync(token, "MetaFrm");
@@ -241,7 +156,8 @@ namespace MetaFrm.Service
             }
             catch (Exception exception)
             {
-                Factory.Logger.LogError(exception, "{Message}", exception.Message);
+                if (Factory.Logger.IsEnabled(LogLevel.Error))
+                    Factory.Logger.LogError(exception, "{Message}", exception.Message);
                 throw new MetaFrmException(exception);
             }
         }
